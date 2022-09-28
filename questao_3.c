259caf7bab11136d12dbde1fa74a66bd735bf010
Bebeto03 <rnxroberto@gmail.com>
2022-09-28 15:22:57 -0300

    resolução questão 3

--- questao_3.c
+++ questao_3.c
@@ -1,1 +1,13 @@
+//Faça um programa capaz de ler um valor real e escrevê-lo com apenas uma casa decimal.
 
+#include <stdio.h>
+int main(){
+  double x;
+
+  printf("Informe um valor real: ");
+  scanf("%lf", &x);
+
+  printf("%.1lf", x);
+
+  return 0;
+}
