5eac95875f42b3f27b6b0965363516490574efa0
Bebeto03 <rnxroberto@gmail.com>
2022-09-28 15:27:10 -0300

    resolução questao 5

--- questao_5.c
+++ questao_5.c
@@ -1,1 +1,16 @@
+//Escreva um programa que pegue o valor de uma conta de restaurante e imprima o valor total a ser pago, considerando que o restaurante cobra 10% de taxa para o garçom.
 
+#include <stdio.h>
+int main(){
+  double x, y;
+  
+  printf("Informe o valor da conta: R$");
+  scanf("%lf", &x);
+
+  y = x + ((10*x)/100);
+
+  printf("Valor a ser pago: R$%.2lf", y);
+  
+
+  return 0;
+}
