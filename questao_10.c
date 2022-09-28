2e3b3e538a9a7506799b4dc09859e64ae43a60cf
Bebeto03 <rnxroberto@gmail.com>
2022-09-28 15:31:54 -0300

    resolução questão 10

--- questao_10.c
+++ questao_10.c
@@ -1,1 +1,23 @@
+//Escreva um programa que calcule e apresente o valor do volume de uma caixa retangular utilizando a fórmula volume = comprimento*laargura*altura
 
+#include <stdio.h>
+int main(){
+  double com, lar, alt, vol;
+
+  printf("Informe o comprimento da caixa: ");
+  scanf("%lf", &com);
+
+  printf("Informe a largura da caixa: ");
+  scanf("%lf", &lar);
+
+  printf("Informe a altura da caixa: ");
+  scanf("%lf", &alt);
+
+  if (com > 0 && lar > 0 && alt > 0){
+    vol = com * lar * alt;
+    printf("Volume da caixa: %.1lf", vol);
+  }else
+    printf("Não é possível realizar esse cálculo");
+  
+  return 0;
+}
