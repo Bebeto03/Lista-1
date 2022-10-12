e7145458f3c9fb4b8e9029e06ac8471825bc6bc1
Bebeto03 <rnxroberto@gmail.com>
2022-10-12 14:48:09 -0300

    resolução

--- questao_11.c
+++ questao_11.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main(){
+  double cot, real;
 
+  printf("Informe a cotação do dólar:R$");
+  scanf("%lf", &cot);
+
+  printf("Informe a quantidade que deseja converter:R$");
+  scanf("%lf", &real);
+
+  printf("Convertido: US$%.2lf", real/cot);
+
+  return 0;
+}
