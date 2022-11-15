50dc5c15dfad3c7491c58203c4201fad6b8bb9cb
Bebeto03 <rnxroberto@gmail.com>
2022-11-15 20:23:10 -0300

    resposta

--- questao_1.c
+++ questao_1.c
@@ -1,1 +1,3 @@
-
+// x = 7+3*6/2-1 : multiplicação -> divisão -> soma -> subtração. Resutado: 15.
+// x = 2%2+2*2-2/2 : primeiro são realizadas as operações de módulo, multiplicação e divisão, depois é realizada a soma e por ultimo a subtração. Resultado: 3.
+// x = (3*9*(3+(9*3/(3)))) : multiplicação à direita -> divisão -> soma -> multiplicações restantes(da esquerda para direita). Resultado: 324.
