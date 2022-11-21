08d59d875516f50d6ecfeed6815e90c7dffbb3fc
Bebeto03 <rnxroberto@gmail.com>
2022-11-21 20:50:55 -0300

    resolução

--- questao_26.c
+++ questao_26.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+  float x1, y1, x2, y2;
+
+  printf("Informe as coordenadas do primeiro ponto:\n");
+  
+  printf("x: ");
+  scanf("%f", &x1);
+  printf("y: ");
+  scanf("%f", &y1);
+
+  printf("Informe as coordenadas do segundo ponto:\n");
+
+  printf("x: ");
+  scanf("%f", &x2);
+  printf("y: ");
+  scanf("%f", &y2);
+
+  printf("A distância entre esses dois pontos no plano é de: %f", sqrt(pow(x2-x1,2.0)+pow(y2-y1,2.0)));
+
+  return 0;
+}
--- questao_27.c
+++ questao_27.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+  float n1, n2, n3;
+
+  printf("Informe um número: ");
+  scanf("%f", &n1);
+
+  printf("Informe outro número: ");
+  scanf("%f", &n2);
+
+  printf("Informe outro número: ");
+  scanf("%f", &n3);
+
+  printf("Média aritmética: %f\n", (n1 + n2 + n3)/3);
+
+  printf("Média geométrica: %f\n", pow(n1*n2*n3, 1.0/3.0));
+
+  return 0;
+}
