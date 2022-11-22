a27ef7a024bd266f44a845a173c9ff35052ef88c
Bebeto03 <rnxroberto@gmail.com>
2022-11-21 21:08:17 -0300

    resolução

--- questao_20.c
+++ questao_20.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
-  int x1, x2, y1, y2;
-  double d;
+  int n;
 
-  printf("Informe a coordenada x do primeiro ponto: ");
-  scanf("%d", &x1);
+  printf("Informe um valor inteiro: ");
+  scanf("%d", &n);
 
-  printf("Informe a coordenada y do primeiro ponto: ");
-  scanf("%d", &y1);
+  switch (n%2){
+    case 0 :
+    printf("Esse valor é um numero par.\n");
+    break;
 
-  printf("Informe a coordenada x do segundo ponto: ");
-  scanf("%d", &x2);
-
-  printf("Informe a coordenada y do segundo ponto: ");
-  scanf("%d", &y2);
-
-  d = sqrt(pow((x1 - x2),2) + pow((y2 - y1),2));
-
-  printf("Distância: %.1lf", d);
+    default :
+    printf("Esse valor é um numero impar.\n");
+  }
 
   return 0;
 }
