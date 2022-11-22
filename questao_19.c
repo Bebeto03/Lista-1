9964ed85fa3a83659e08b6e782b9489541b3893a
Bebeto03 <rnxroberto@gmail.com>
2022-11-21 21:44:47 -0300

    resolução

--- questao_19.c
+++ questao_19.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+
+    printf("********\t\t    ***    \t\t   *   \t         *          \n");
+    printf("*      *\t\t  *     *  \t\t  ***  \t       *   *        \n");
+    printf("*      *\t\t*         *\t\t ***** \t     *       *      \n");
+    printf("*      *\t\t*         *\t\t   *   \t   *           *    \n");
+    printf("*      *\t\t*         *\t\t   *   \t *               *  \n");
+    printf("*      *\t\t*         *\t\t   *   \t   *           *    \n");
+    printf("*      *\t\t*         *\t\t   *   \t     *       *      \n");
+    printf("*      *\t\t  *     *  \t\t   *   \t       *   *        \n");
+    printf("********\t\t    ***    \t\t   *   \t         *          \n");
+
+    return 0;
+    
+}
