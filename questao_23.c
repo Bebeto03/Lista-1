b94bd80dbd67a29d33fe80e251727d50935e5592
Bebeto03 <rnxroberto@gmail.com>
2022-11-21 23:50:48 -0300

    resolução

--- questao_23.c
+++ questao_23.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(){
+    int x, d1, d2, d3;
+
+    printf("Informe um numero de tres digitos: ");
+    scanf("%d", &x);
+
+    d1 = x / 100;
+    d2 = (x%100)/10;
+    d3 = (x%100)%10;
+
+    printf("%d%d%d", d3, d2, d1);
+
+
+
+    return 0;
+}
