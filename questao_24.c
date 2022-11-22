82762e77923f3deae6ff131a0a45fe482870ed3d
Bebeto03 <rnxroberto@gmail.com>
2022-11-22 00:10:07 -0300

    resolução

--- questao_24.c
+++ questao_24.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+  int n;
+  float x;
+
+  printf("Informe o valor de x: ");
+  scanf("%f", &x);
+
+  printf("Informe o valor de n: ");
+  scanf("%d", &n);
+
+  printf("%f", x*pow(2,n));
+
+  return 0;
+}
--- questao_25.c
+++ questao_25.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(){
+    int x, d1, d2, d3;
+
+    printf("Informe um tempo, em segundos: ");
+    scanf("%d", &x);
+
+    d1 = x / 3600;
+    d2 = (x%3600)/60;
+    d3 = (x%3600)%60;
+
+    printf("%dh:%dm:%ds", d1, d2, d3);
+
+
+
+    return 0;
+}
