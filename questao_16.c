8f45d669eba9a39f8136ee555d5e418ef04a5e69
Bebeto03 <rnxroberto@gmail.com>
2022-11-21 21:27:37 -0300

    resolução

--- questao_16.c
+++ questao_16.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(){
+    int ht;
+    float vha, per;
+
+    printf("Informe o valor da hora aula: ");
+    scanf("%f", &vha);
+
+    printf("Informe o numero de horas trabalhadas no mes: ");
+    scanf("%d", &ht);
+
+    printf("Informe o percentual de desconto do INSS: ");
+    scanf("%f", &per);
+    printf("% \n");
+
+    printf("Salario bruto: R$%.2f\n", vha * ht);
+    printf("Salario liquido: R$%.2f\n", (100*(vha*ht)-per*(vha*ht))/100);
+
+    return 0;
+}
