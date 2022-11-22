866a478a5e85a85e68258ece55d065fc7949e123
Bebeto03 <rnxroberto@gmail.com>
2022-11-21 21:39:39 -0300

    resolução

--- questao_18.c
+++ questao_18.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(){
+    float raio;
+    
+    printf("CIRCULO:\n");
+
+    printf("Informe o valor do raio: ");
+    scanf("%f", &raio);
+
+    printf("Diametro: %f\n", 2*raio);
+    printf("Circunferencia: %f\n", 2*raio*3.14159);
+    printf("Area: %f\n", raio*raio*3.14159);
+
+    return 0;
+}
