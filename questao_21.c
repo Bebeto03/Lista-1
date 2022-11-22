41e5fc75bbdee926f8348d3f9a17faae4178b5da
Bebeto03 <rnxroberto@gmail.com>
2022-11-21 23:19:08 -0300

    resolução

--- questao_21.c
+++ questao_21.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(){
+    int x, y;
+
+    printf("Informe um valor inteiro: ");
+    scanf("%d", &x);
+
+    printf("Informe um segundo valor inteiro: ");
+    scanf("%d", &y);
+
+    switch (x%y){
+        case 0 :
+        printf("%d é multiplo de %d", x,y);
+        break;
+        
+        default :
+        printf("%d nao é multiplo de %d", x,y);
+    }
+
+    return 0;
+}
